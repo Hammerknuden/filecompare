e9657e9063a77670c91c4c687defdbcada642f91
Finn <finnjorg@>
2025-05-12 17:21:04 +0200

    commit by 12 maj 2025 1720

--- filecompare.py
+++ filecompare.py
@@ -1,1 +1,10 @@
 import streamlit as st
+st.text('compare URL file names')
+file_1 = st.text_input("indsæt file 1  HTTPS://  ")
+file_2 = st.text_input("indsæt file 2  HTTPS://  ")
+
+if file_1 == file_2:
+    st.markdown("Files are identical  ")
+else:
+    st.markdown("Files are not identical")
+
